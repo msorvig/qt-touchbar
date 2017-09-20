1f0e145abd506d74bc0d4e15cf91a457bb24c567
Morten Johan Sørvig <morten.sorvig@qt.io>
2017-09-20 14:08:24 +0200

    Set up forwarding from user delegates to Qt delegates
    We want to install NSApplication and/or NSWindow delegates
    that implement the NSTouchBarDelegate protocol. However,
    Qt already has delegates which we would then override.
    
    Solve this by setting up forwarding from the user
    delegate to the Qt delegate, implemented using 
    -[NSObject respondsToSelector] and -[NSObject forwardInvocation].
    
    This should work with current Qt versions.

--- main.mm
+++ main.mm
@@ -1,23 +1,29 @@
 #include <QtWidgets>
 #import <AppKit/AppKit.h>
 
-// This example shows how to create and populate a global touch bar
-// for a Qt application. The touch bar will be active for all application
-// windows.
+// This example shows how to create and populate touch bars for Qt applications.
+// Two approaches are demonstrated: creating a global touch bar for the entire
+// application via the NSApplication delegate, and creating per-window touch bars
+// via the NSWindow delegate. Applications may use either or both of these, for example
+// to provide global base touch bar with window specific additions. Refer to the
+// NSTouchBar documentation for further details.
+
+// The TouchBarProvider class implements the NSTouchBarDelegate protocol, as
+// well as app and window delegate protocols.
+@interface TouchBarProvider: NSResponder <NSTouchBarDelegate, NSApplicationDelegate, NSWindowDelegate>
+
+@property (strong) NSCustomTouchBarItem *touchBarItem1;
+@property (strong) NSCustomTouchBarItem *touchBarItem2;
+@property (strong) NSButton *touchBarButton1;
+@property (strong) NSButton *touchBarButton2;
+
+@property (strong) NSObject *qtDelegate;
+
+@end
 
 // Create identifiers for two button items.
 static NSTouchBarItemIdentifier Button1Identifier = @"com.myapp.Button1Identifier";
 static NSTouchBarItemIdentifier Button2Identifier = @"com.myapp.Button2Identifier";
-
-// We will be replacing the global NSApplication delegate with an instance
-// of this TouchBarProvider class. The class implements makeTouchBar which
-// creates the touch bar.
-@interface TouchBarProvider: NSResponder <NSTouchBarDelegate, NSApplicationDelegate>
-
-@property (strong) NSCustomTouchBarItem *touchbarButton1;
-@property (strong) NSCustomTouchBarItem *touchbarButton2;
-
-@end
 
 @implementation TouchBarProvider
 
@@ -26,12 +32,12 @@ static NSTouchBarItemIdentifier Button2Identifier = @"com.myapp.Button2Identifie
     // Create the touch bar with this instance as its delegate
     NSTouchBar *bar = [[NSTouchBar alloc] init];
     bar.delegate = self;
-    
+
     // Add touch bar items: first, the very important emoji picker, followed
-    // by a couple of buttons. Note that no further handling of the emoji picker
+    // by two buttons. Note that no further handling of the emoji picker
     // is needed (emojii are automatically routed to any active text edit). Button
-    // activation is handled in makeItemForIdentifier below.
-    bar.defaultItemIdentifiers = @[NSTouchBarItemIdentifierCharacterPicker, 
+    // actions handlers are set up in makeItemForIdentifier below.
+    bar.defaultItemIdentifiers = @[NSTouchBarItemIdentifierCharacterPicker,
                                    Button1Identifier, Button2Identifier];
 
     return bar;
@@ -39,21 +45,52 @@ static NSTouchBarItemIdentifier Button2Identifier = @"com.myapp.Button2Identifie
 
 - (NSTouchBarItem *)touchBar:(NSTouchBar *)touchBar makeItemForIdentifier:(NSTouchBarItemIdentifier)identifier
 {
-    Q_UNUSED(touchBar); 
-    
+    Q_UNUSED(touchBar);
+
     // Create touch bar items as NSCustomTouchBarItems which can contain any NSView.
     if ([identifier isEqualToString:Button1Identifier]) {
-        _touchbarButton1 = [[NSCustomTouchBarItem alloc] initWithIdentifier:identifier];
-        _touchbarButton1.view = [[NSButton buttonWithTitle:@"B1" target:self
-                                           action:@selector(button1Clicked)] autorelease];
-         return self.touchbarButton1;
+        QString title = "B1";
+        self.touchBarItem1 = [[[NSCustomTouchBarItem alloc] initWithIdentifier:identifier] autorelease];
+        self.touchBarButton1 = [[NSButton buttonWithTitle:title.toNSString() target:self
+                                          action:@selector(button2Clicked)] autorelease];
+        self.touchBarItem1.view =  self.touchBarButton1;
+         return self.touchBarItem1;
     } else if ([identifier isEqualToString:Button2Identifier]) {
-        _touchbarButton2 = [[NSCustomTouchBarItem alloc] initWithIdentifier:identifier];
-        _touchbarButton2.view = [[NSButton buttonWithTitle:@"B2" target:self
-                                           action:@selector(button2Clicked)] autorelease];
-        return self.touchbarButton2;
+        QString title = "B2";
+        self.touchBarItem2 = [[[NSCustomTouchBarItem alloc] initWithIdentifier:identifier] autorelease];
+        self.touchBarButton2 = [[NSButton buttonWithTitle:title.toNSString() target:self
+                                          action:@selector(button2Clicked)] autorelease];
+        self.touchBarItem2.view =  self.touchBarButton2;
+        return self.touchBarItem2;
     }
    return nil;
+}
+
+- (void)installAsDelegateForWindow:(NSWindow *)window
+{
+    _qtDelegate = window.delegate; // Save current delegate for forwarding
+    window.delegate = self;
+}
+
+- (void)installAsDelegateForApplication:(NSApplication *)application
+{
+    _qtDelegate = application.delegate; // Save current delegate for forwarding
+    application.delegate = self;
+}
+
+- (BOOL):(SEL)aSelector
+{
+    // We want to forward to the qt delegate. Respond to selectors it
+    // responds to in addition to selectors this instance resonds to.
+    return [_qtDelegate respondsToSelector:aSelector] || [super respondsToSelector:aSelector];
+}
+
+- (void)forwardInvocation:(NSInvocation *)anInvocation
+{
+    // Forward to the existing delegate. This function is only called for selectors
+    // this instance does not responds to, which means that the qt delegate
+    // must respond to it (due to the respondsToSelector implementation above).
+    [anInvocation invokeWithTarget:_qtDelegate];
 }
 
 - (void)button1Clicked
@@ -65,32 +102,29 @@ static NSTouchBarItemIdentifier Button2Identifier = @"com.myapp.Button2Identifie
 {
     qDebug() << "button2Clicked";
 }
-    
+
 @end
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
 
-    QTextEdit *textEdit = nullptr;
-    TouchBarProvider *touchBarProvider = nil;
-
-    QTimer::singleShot(0, [&textEdit, &touchBarProvider](){
-        textEdit = new QTextEdit();
-        textEdit->show();
-
+    {
+        // Install TouchBarProvider as application delegate
         TouchBarProvider *touchBarProvider = [[TouchBarProvider alloc] init];
-        
-        // Replace the global application delegate with our delegate. Note
-        // that this removes the Qt internal application delegate. 
-        // (TODO: investigate if Qt really needs an app delegate)
-        [NSApplication sharedApplication].delegate = touchBarProvider;
-    });
-		
-    int code = app.exec();
+        [touchBarProvider installAsDelegateForApplication:[NSApplication sharedApplication]];
+    }
 
-    delete textEdit;
-    [touchBarProvider release];
-    return code;
+    QTextEdit textEdit;
+    textEdit.show();
+
+    {
+        // Install TouchBarProvider as window delegate
+        NSView *view = reinterpret_cast<NSView *>(textEdit.winId());
+        TouchBarProvider *touchBarProvider = [[TouchBarProvider alloc] init];
+        [touchBarProvider installAsDelegateForWindow:view.window];
+    }
+
+    return app.exec();
 }
-	
+
